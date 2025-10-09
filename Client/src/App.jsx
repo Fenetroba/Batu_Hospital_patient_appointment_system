58fb81869450d94794dd3261e8fb245a3b11b7bd
fenet roba <fenetroba700@gmail.com>
2025-10-09 15:29:20 +0300

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './Components/ui/button'
+import HomePage from './Page/Gusts/HomePage'
+
+const App = () => {
+  return (
+   <>
+   <HomePage/>
+   
+   </>
+  )
+}
+
+export default App
