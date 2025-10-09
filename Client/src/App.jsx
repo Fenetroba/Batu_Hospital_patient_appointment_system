42b5c95810c38efb547129e635b33a67ab42ca45
fenet roba <fenetroba700@gmail.com>
2025-10-09 16:17:43 +0300

    updated navigationbar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,10 +4,10 @@ import HomePage from './Page/Gusts/HomePage'
 
 const App = () => {
   return (
-   <>
+   <div className='bg-gradient-to-b from-[var(--one)] to-[var(--two)] h-[100dvh]'>
    <HomePage/>
    
-   </>
+   </div>
   )
 }
 
--- Client/src/Page/Gusts/Header.jsx
+++ Client/src/Page/Gusts/Header.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="shadow-lg">
+      <div className="flex items-center justify-between px-8">
+        <p className="p-2 font-extrabold text-2xl ">
+          BATU <span className="text-[var(--five)]">HOSPITAL</span>
+        </p>
 
-export default Header
+      <div className="flex gap-5 items-center">
+        <p>Home</p>
+      <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>ABOUT US</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>LOCATION</NavigationMenuLink>
+                <NavigationMenuLink>INFORMATION</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>OUR SERVICE</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink  className='w-[100px]'>With Email</NavigationMenuLink>
+                <NavigationMenuLink>With Phone</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
