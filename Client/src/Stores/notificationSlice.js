4eee4affef8ccdcaec43abdaf33dbfbd050b5450
fenet roba <fenetroba700@gmail.com>
2025-12-04 10:47:19 +0300

    Update notificationSlice.js

--- Client/src/Stores/notificationSlice.js
+++ Client/src/Stores/notificationSlice.js
@@ -1,7 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
-
-const API_URL = 'http://localhost:5000/api/notification'
+import axios from '../Lib/Axios'
 
 // Fetch all notifications
 export const fetchNotifications = createAsyncThunk(
