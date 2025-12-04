588826da7997dc4326853645431a9a91459e13ea
fenet roba <fenetroba700@gmail.com>
2025-12-04 09:57:10 +0300

    Update Axios base URL to production server

--- Client/src/Lib/Axios.js
+++ Client/src/Lib/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with base URL
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://batu-hospital-patient-appointment-system.onrender.com/api",
     withCredentials:true,
     headers:{
         "Content-Type":"application/json"
